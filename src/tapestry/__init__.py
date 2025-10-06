a9d17410bcd36b467069fbcbf3bcdf83de186611
ze97286 <zohar.etzioni@gmail.com>
2025-10-06 12:40:34 +0100

    feat: tapestry data loading and aggregation

--- src/tapestry/__init__.py
+++ src/tapestry/__init__.py
@@ -8,12 +8,12 @@ __version__ = "0.1.0"
 __author__ = "Zohar Etzioni"
 
 from .data import loader, regions, aggregator
-from .model import vae, losses
+# from .model import vae, losses
 
 __all__ = [
     'loader',
     'regions', 
     'aggregator',
-    'vae',
-    'losses',
+    # 'vae',
+    # 'losses',
 ]
